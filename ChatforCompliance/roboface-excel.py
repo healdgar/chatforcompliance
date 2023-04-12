ede9898ed89fe27e15c0912d4cdfed754ad3c091
healdgar <alexwall77@gmail.com>
2023-04-11 18:34:57 -0700

    turn on and off excel question detection detect if Excel is open before crashing more color use in CMD output for watching progress indicator

--- ChatforCompliance/roboface-excel.py
+++ ChatforCompliance/roboface-excel.py
@@ -3,8 +3,30 @@ import re
 import os
 from roboface import write_answer
 
-# Define the Excel file name
-excel_filename = input('Please enter the filename of the Excel that you placed in the "Excels" folder: ')
+# Define the Excel file name.  Permit the user to enter the file name without the extension.  Provide appropriate error messages if the file is not found.
+while True:
+    excel_filename = input('Please enter the filename of the Excel that you placed in the "Excels" folder: ')
+    file_ext = os.path.splitext(excel_filename)[-1].lower()
+
+    if file_ext == "":
+        if os.path.isfile(f"Excels/{excel_filename}.xlsx"):
+            excel_filename += ".xlsx"
+            break
+        elif os.path.isfile(f"Excels/{excel_filename}.xls"):
+            excel_filename += ".xls"
+            break
+        else:
+            print("File not found. Please make sure the file is in the 'Excels' folder and try again.")
+    elif file_ext in ['.xlsx', '.xls']:
+        if os.path.isfile(f"Excels/{excel_filename}"):
+            break
+        else:
+            print("File not found. Please make sure the file is in the 'Excels' folder and try again.")
+    else:
+        print("Invalid file extension. Please enter a valid Excel filename.")
+
+
+source = f"roboface-excel.py, excel_filename={excel_filename}"
 
 # Construct the full file path
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,8 +36,39 @@ excel_filepath = os.path.join(excels_dir, excel_filename)
 
 # Load the Excel file
 print("Loading the Excel file...")
-xlsx = pd.read_excel(excel_filepath, sheet_name=None, header=None)
 
+try:
+    xlsx = pd.read_excel(excel_filepath, sheet_name=None, header=None)
+except PermissionError:
+    print("Excel file is open in another program. Please close the file and hit 'Y' to continue.")
+    while True:
+        user_input_qdetect = input("Continue? (Y/N): ")
+        if user_input_qdetect.lower() == "y":
+            break
+        elif user_input_qdetect.lower() == "n":
+            exit()
+        else:
+            print("Please enter Y or N.")
+            continue  
+print("Excel file loaded.")
+
+#check whether user wishes to use English language question detection regex
+while True:
+    user_input_qdetect = input("Use English language question detection regex? (Y/N): ")
+    if user_input_qdetect.lower() == "y":
+        break
+    elif user_input_qdetect.lower() == "n":
+        break
+    else:
+        print("Please enter Y or N.")
+        continue
+
+# Check if the 'Roboface QRA' sheet exists and remove it if it does
+with pd.ExcelWriter(excel_filepath, engine='openpyxl', mode='a') as writer:
+    if 'Roboface QRA' in writer.book:
+        writer.book.remove(writer.book['Roboface QRA'])
+
+# Define a function to identify questions
 def is_question(sentence):
     pattern = r'\b(what|where|when|why|how|which|who|whom|whose)\b.*|\byou(r)?\b.*|\?.*|\bplease\b.*|\b(provide|describe)\b.*|Name of '
     match = re.search(pattern, sentence, re.MULTILINE)
@@ -36,28 +89,55 @@ print("Processing sheets...")
 for sheet_name, df in xlsx.items():
     print(f"Processing sheet: {sheet_name}")
     
-    # Identify cells containing questions
+    # Identify cells containing questions and do not process blank cells as questions
     questions = []
     for row in range(df.shape[0]):
         for col in range(df.shape[1]):
-            if pd.notna(df.iloc[row, col]) and is_question(str(df.iloc[row, col])):
-                questions.append((df.iloc[row, col], row, col))
+            cell_value = df.iloc[row, col]
+            if pd.notna(cell_value):
+                if user_input_qdetect.lower() == "y":
+                    if is_question(str(cell_value)):
+                        questions.append((cell_value, row, col))
+                else:
+                    questions.append((cell_value, row, col))
+
 
     # Process the questions and write the questions, answers, and cell locations to the DataFrame
-    for question, row, col in questions:
-        # Replace 'fake_write_answers' with the function you use to generate answers
-        answer = write_answer(question)
+    for index, (question, row, col) in enumerate(questions):
+        # write answer is passing a source parameter as well for logging purposes
+        answer = write_answer(question, source)
         cell = cell_coordinates(row, col)
         new_row = pd.DataFrame({"Sheet Name": [sheet_name], "Cell": [cell], "Question": [question], "Answer": [answer]})
-        qa_df = pd.concat([qa_df, new_row], ignore_index=True)        
-    
-    print(f"Finished processing sheet: {sheet_name}")
+        
+        # Append the new_row to the qa_df DataFrame
+        qa_df = pd.concat([qa_df, new_row], ignore_index=True)
 
+        # Display progress information
+        remaining_questions = len(questions) - index - 1
+        progress_message = f"Processing Question {index + 1} of {len(questions)} ({remaining_questions} remaining)"
+        print("\033[31m" + progress_message + "\033[0m")
+        print("Current Row:", row, "Column:", col)
+
+
+    print(f"Finished processing sheet: {sheet_name}.  Overwriting Roboface QRA sheet in Excel file if it exists...")
+
+
+#check to see if Excel file is open in another program
+try:
+    with pd.ExcelWriter(excel_filepath, engine='openpyxl', mode='a') as writer:
+        qa_df.to_excel(writer, sheet_name='Roboface QRA', index=False)
+except PermissionError:
+    print("Excel file is open in another program. Please close the file and hit 'Y' to continue.")
+    while True:
+        user_input = input("Continue? (Y/N): ")
+        if user_input.lower() == "y":
+            break
+        elif user_input.lower() == "n":
+            exit()
+        else:
+            print("Please enter Y or N.")
+            continue    
 print("Writing questions, answers, and cell locations to a new sheet...")
-
-# Write the DataFrame to a new sheet in the Excel file
-with pd.ExcelWriter(excel_filepath, engine='openpyxl', mode='a') as writer:
-    qa_df.to_excel(writer, sheet_name='Roboface QRA', index=False)
 
 # Print completion message
 print("All questions and answers, along with their cell locations, have been written to a new sheet in the Excel file.  Output has also be recorded to the data.csv file found in the 'config' folder.")
